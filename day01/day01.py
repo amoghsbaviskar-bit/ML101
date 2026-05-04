e8051bbc8c68f94ad624de8fc4b61ab6401c832d
amoghsbaviskar-bit <amoghsbaviskar@gmail.com>
2026-05-04 17:48:59 +0530

    ADDED IMG OF THE OUTPUT GRAPH AND OUTPUT IN output.txt and USED matplotlib.pyplot to get the graph

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,11 @@
 import torch
+import matplotlib.pyplot as plt 
 
 x = torch.tensor([2.0,4.0])
 y = torch.tensor([4.0,6.0])
+
+
+losses = [] 
 
 w = torch.tensor([[0.0]], requires_grad=True)
 b = torch.tensor([[0.0]], requires_grad=True)
@@ -19,3 +23,13 @@ for epoch in range(200):
     b.grad.zero_() 
     
     print(f"Epoch {epoch}, Loss: {loss.item()}, w: {w.item()}, b: {b.item()}")
+    losses.append(loss.item())
+    
+plt.plot(losses)
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Training Loss Curve")
+
+plt.savefig("loss.png")
+
+plt.show()
