fbc782ec248450e81e4c58651a9672f869fd73c9
amoghsbaviskar-bit <amoghsbaviskar@gmail.com>
2026-05-13 13:44:15 +0530

    day 05 commit

---
+++ day04/day04.py
@@ -0,0 +1,52 @@
+import torch as th
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as mlt
+
+x = th.tensor([[2.0],[4.0]])
+y = th.tensor([[1.0],[3.0]])
+
+class neuronNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(1,10)
+        
+        self.relu = nn.ReLU()
+        
+        self.layer2 = nn.Linear(10,1)
+    
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        
+        return x
+    
+model = neuronNet()
+
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.01)
+
+losses = []
+
+for epoch in range (1000):
+    y_pred = model(x)
+    loss = criterion(y_pred,y)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    losses.append(loss.item())
+    if epoch % 100 == 0:
+        print(f"Epoch:{epoch} , Loss:{loss.item()}")
+    
+mlt.plot(losses)
+mlt.xlabel("Epoch")
+mlt.ylabel("Loss")
+mlt.title("Training Loss Curve")
+
+mlt.savefig("loss.png")
+
+mlt.show()
---
+++ day04/what_i_did.md
@@ -0,0 +1,27 @@
+# Day - 04 My First Neural Network
+
+## What I did 
+* Implemented Neural Networking to the day03 program
+* started using 2d Tensor
+* Switched from hard coding Formulas to using nn.Linear() and nn.ReLU()
+* Increseaed the epoch range to 1000
+* Used lr = 0.01 instead of 0.1 so as to not get NaN
+
+---
+
+## Output
+* 'output.txt' : shows the output I got after running the code 
+* 'loss.png' : shows the loss graph 
+
+---
+
+## Observation 
+* I found out that unlike the day03 loss graph which was very bumpy and then became stable this graph became stable after one small bump
+* using nn.linear is better than having to hard code formulas 
+
+---
+
+** Learning from today
+* The system can make it's own weights and biases and we dont need to define them
+
+---
